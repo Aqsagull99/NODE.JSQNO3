6dfed72c07fe2ac664dcea99cda609878783a2da
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:06:09 +0500

    Add files via upload

---
+++ qno3.js
@@ -0,0 +1,6 @@
+// //   QNO:3
+// // Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// let myName:string="aqsa gull"
+// console.log("lowercase",myName.toLowerCase())
+// console.log("uppercase",myName.toUpperCase())
+// console.log("titlecase",myName.replace(/\b\w/g,(char)=> char.toUpperCase()));
